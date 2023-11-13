7f90403a047859cd058953f5c71fea6e5247ac29
jinchenxing <2541226493@qq.com>
2023-11-13 23:56:28 +0800

    作者模块开发

--- src/views/profile/components/Author.vue
+++ src/views/profile/components/Author.vue
@@ -1,7 +1,46 @@
 <template>
-  <div>Author</div>
+  <div class="author-container">
+    <div class="header">
+      <my-avatar
+        image="https://public-cdn-oss.mosoteach.cn/avatar/2019/4C/3117093603a057f6bf0e5dd65aa5b8ff.jpg?v=1676265419"
+        height="60px"
+        width="60px"
+      >
+      </my-avatar>
+      <div class="header-desc">
+        <h3>Flobby</h3>
+        <span>计算机与软件学院</span>
+      </div>
+    </div>
+    <div class="info">Web 全栈开发工程师</div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import MyAvatar from '@/components/MyAvatar/index.vue'
+import {} from 'vue'
+</script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.author-container {
+  .header {
+    display: flex;
+    .header-desc {
+      margin-left: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+  .info {
+    margin-top: 16px;
+    line-height: 22px;
+    font-size: 14px;
+    text-indent: 26px;
+  }
+}
+</style>
