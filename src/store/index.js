b3ad0108a3cdf903b1709629882ba8e61b2dcd19
jinchenxing <2541226493@qq.com>
2023-11-13 22:54:45 +0800

    创建 SelectColor 组件

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import { createStore } from 'vuex'
 import user from './modules/user.js'
 import getters from './getters'
 import app from './modules/app'
-
+import theme from './modules/theme'
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
   getters,
-  modules: { user, app }
+  modules: { user, app, theme }
 })
