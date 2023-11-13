589b5a770b842d10335a026569eed7b41d04bfdf
jinchenxing <2541226493@qq.com>
2023-11-13 16:53:22 +0800

    左侧菜单伸缩功能实现

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import { createStore } from 'vuex'
 import user from './modules/user.js'
 import getters from './getters'
+import app from './modules/app'
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
   getters,
-  modules: { user }
+  modules: { user, app }
 })
