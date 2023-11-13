f5cdb4e85a2aa5a7e215ca516d655f8d95c820c4
jinchenxing <2541226493@qq.com>
2023-11-13 22:57:29 +0800

    处理 element-plus 主题变更

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>profile</div>
+  <div>
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import {} from 'vue'
+</script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
