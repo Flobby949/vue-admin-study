5b5a5c0543c3bdc2f39267581105f0a032828acc
jinchenxing <2541226493@qq.com>
2023-11-13 16:01:29 +0800

    feat: 登录后处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -43,7 +43,7 @@
 import { ref } from 'vue'
 import { validatePassword } from './rules'
 import { useStore } from 'vuex'
-
+import { useRouter } from 'vue-router'
 // 数据源
 const loginForm = ref({
   username: 'admin',
@@ -82,6 +82,7 @@ const onChangePwdType = () => {
 const loading = ref(false)
 const loginFormRef = ref(null)
 const store = useStore()
+const router = useRouter()
 const handleLogin = () => {
   loginFormRef.value.validate((valid) => {
     if (!valid) return
@@ -91,6 +92,7 @@ const handleLogin = () => {
       .then(() => {
         loading.value = false
         // TODO: 登录后操作
+        router.push('/')
       })
       .catch((err) => {
         console.log(err)
