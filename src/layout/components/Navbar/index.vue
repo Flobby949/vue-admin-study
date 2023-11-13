3b6298019c0430d725c2f6a8f81282b8e43d7c39
jinchenxing <2541226493@qq.com>
2023-11-13 16:59:42 +0800

    渲染基本的面包屑组件

--- src/layout/components/Navbar/index.vue
+++ src/layout/components/Navbar/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="navbar">
     <hamburger class="hamburger-container" />
+    <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
@@ -28,6 +29,7 @@
 import {} from 'vue'
 import { useStore } from 'vuex'
 import Hamburger from '@/components/Hamburger'
+import Breadcrumb from '@/components/Breadcrumb'
 
 const store = useStore()
 const logout = () => {
@@ -42,6 +44,10 @@ const logout = () => {
   position: relative;
   background: #fff;
   box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+
+  .breadcrumb-container {
+    float: left;
+  }
 
   .hamburger-container {
     line-height: 46px;
