89736e972e60e71a505d0e71097400b99b76aa10
jinchenxing <2541226493@qq.com>
2023-11-14 17:45:28 +0800

    feat(custom): 创建文章基本结构实现

---
+++ src/views/article-create/components/Editor.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>editor</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/views/article-create/components/Markdown.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>markdown</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
